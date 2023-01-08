66a9bcee942b282ec7805fafbe0c845d59ebdcde
Chinmoy Hembram <53371290+hunterjc@users.noreply.github.com>
2023-01-09 00:58:44 +0530

    Update client.js

--- public/client.js
+++ public/client.js
@@ -2,6 +2,7 @@ const socket = io()
 let name;
 let textarea = document.querySelector('#textarea')
 let messageArea = document.querySelector('.message__area')
+let btn = document.getElementById("button");
 do {
     name = prompt('Please enter your name: ')
 } while (!name)
